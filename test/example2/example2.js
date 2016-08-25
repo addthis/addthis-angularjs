4aabfae77b16d50c0972f14f42959143bdff75ea
Julka Grodel <julka@addthis.com>
2016-08-25 17:37:59 -0400

    example using ngRoute

---
+++ test/example2/example2.js
@@ -0,0 +1,21 @@
+var appExample2 = angular.module('appExample2', ['ngRoute', 'official.addthis']);
+
+appExample2.config(function($routeProvider) {
+    $routeProvider
+    .when('/Route1', {
+        templateUrl: 'Route1.html'
+    })
+    .when('/Route2', {
+        templateUrl: 'Route2.html'
+    });
+});
+
+appExample2.config(function($addthisProvider, $windowProvider) {
+    $addthisProvider.profileId("ra-57b71bceb3ebb9df");
+});
+
+appExample2.controller('NavCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.path = function(view) {
+        $location.path(view); // path not hash
+    };
+}]);
