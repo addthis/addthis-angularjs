cc359f3720917644b1c851fb4e4e3d4a103c272f
Julka Grodel <julka@addthis.com>
2016-09-06 12:21:00 -0400

    TitleSwitcherCtrl tests

--- test/example3/TitleSwitcherCtrlTest.js
+++ test/example3/TitleSwitcherCtrlTest.js
@@ -16,4 +16,21 @@ describe('appExample3 TitleSwitcherCtrl', function() {
         $scope = {};
         controller = $controller('TitleSwitcherCtrl', { $scope: $scope });
     }));
+
+    it('should initialize with share title with its initial value', function() {
+        expect($scope.currentTitle).toBe(initialTitle);
+    });
+
+    describe('changeTitle', function() {
+        it('should change the share title to its alternate value when executed once', function() {
+            $scope.changeTitle();
+            expect($scope.currentTitle).toBe(alternateTitle);
+        });
+
+        it('should change the share title to its default value when executed twice', function() {
+            $scope.changeTitle();
+            $scope.changeTitle();
+            expect($scope.currentTitle).toBe(initialTitle);
+        });
+    });
 });
