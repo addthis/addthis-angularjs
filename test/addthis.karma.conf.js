dce928f1d440097aa0b7652f8070e60afbb84bc3
Julka Grodel <julka@addthis.com>
2016-09-20 14:44:21 -0400

    changing comment whitespace

--- test/addthis.karma.conf.js
+++ test/addthis.karma.conf.js
@@ -76,12 +76,11 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
-        //'Chrome'//,
+//        'Chrome',
 //        'Firefox',
 //        'Safari',
         'PhantomJS'
     ],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
