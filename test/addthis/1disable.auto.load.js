d6665dfce805d684091954d71f372595a50dbe1e
Julka Grodel <julka@addthis.com>
2016-09-20 16:08:38 -0400

    test

---
+++ test/addthis/1disable.auto.load.js
@@ -0,0 +1,36 @@
+/* globals describe, inject, beforeEach, afterEach, expect, it, spyOn */
+
+'use strict';
+
+describe('setup', function() {
+    var $addthis;
+
+    var findAddThisScriptOnPage = function(header) {
+        var addthisWidgetSelector;
+        if (header === true) {
+            addthisWidgetSelector = 'script[src*="addthis_widget.js"]';
+        } else {
+            addthisWidgetSelector = 'body script[src*="addthis_widget.js"]';
+        }
+
+        var matches = document.querySelectorAll(addthisWidgetSelector);
+        return matches;
+    };
+
+    describe('after calling $addthisProvider.disable_auto_add()', function() {
+        beforeEach(function() {
+            module(function($addthisProvider) {
+                $addthisProvider.disable_auto_add();
+            });
+        });
+
+        beforeEach(inject(function($injector) {
+            $addthis = $injector.get('$addthis');
+        }));
+
+        it('should not auto add addthis_widget.js', function() {
+            var matches = findAddThisScriptOnPage();
+            expect(matches.length).toBe(0);
+        });
+    });
+});
