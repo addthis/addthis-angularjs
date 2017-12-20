adc52e43b5511eb8e70a2674d666c8f283ea122d
Chen Li <chen.l.li@oracle.com>
2017-12-19 19:01:34 -0500

    add docs folder for github page

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,85 @@
+NG_DOCS={
+  "sections": {
+    "api": "official-addthis-angularjs"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "addthis",
+      "shortName": "addthis",
+      "type": "overview",
+      "moduleName": "addthis",
+      "shortDescription": "Free and Pro AddThis tools to your AngularJS app. This AngularJS module",
+      "keywords": "account addthis affect angularjs api app changes cycles digest directive free includes module overview pro provider requires route service smart tools"
+    },
+    {
+      "section": "api",
+      "id": "addthis.$addthis",
+      "shortName": "addthis.$addthis",
+      "type": "service",
+      "moduleName": "addthis",
+      "shortDescription": "A service for handling AddThis actions once your app is running.",
+      "keywords": "$addthis $addthisprovider $scope $service $window _blank acceptcookiesctrl account accountpagectrl actions add adds addthis addthis_config addthis_share addthis_widget addthisinfoctrl addthistool adipiscing allow amet api app applys attribute automatically bitly browsers buttons call catch cfg changes check clicks config configuration consectetur controller copy crawls data_track_clickback debugger default define description devleoper directive directly disableautoadd document documentation dolor domagicctrl don donotprintoremailmectrl elit enable enables explicitly facebook false finds floating function graph handle handling href http https image inline input ipsum js layersrefresh loaded loadmorecatsctrl location lorem mailto media method module note object open options passthrough pl png print profile profileid promise property pubid ready reflect refreshed reloads remove reset resolves returns running script service services services_exclude set sets setting share share-description share-media share-title share-url share_info shared sharedescription sharemedia sharetitle shareurl sharing shortcut shortening sit site situations siutations smartlayers social socialservice speicfy string tags takes target test thge times title tools true turned twitter twittervia ui_508_compliant ui_language updates url urlshortening urlshorteningservice user useraccepts username values var variable wasn"
+    },
+    {
+      "section": "api",
+      "id": "addthis.$addthisProvider",
+      "shortName": "addthis.$addthisProvider",
+      "type": "service",
+      "moduleName": "addthis",
+      "shortDescription": "A provider for handling AddThis actions before you app has started",
+      "keywords": "$addthis $addthisprovider $envprovider _blank actions add adding adds addthis addthis_config addthis_share addthis_widget addthisprovider addthistool adipiscing allow amet api app append attribute automatically bitly bottom browsers buttons calling cfg change changes check clicks config configuration consectetur copy crawls data_track_clickback debugger default defaults define description dev developers directive disable disableautoadd disables document documentation dolor dom don element elit enable enableautoadd enables env environment environments explicitly facebook false finds firewalls function functionality general graph handle handling href http https image included input inside ipsum isproduction js local lorem mailto manually media method module note object open optional options pl png print production profile profileid provider pubid re-enables remove reset returns running script scriptinhead service services services_exclude set sets setter setting share share-description share-media share-title share-url shared sharedescription sharemedia sharetitle shareurl sharing shortcut shortening sit site social socialservice speicfy started string tags takes target test testing things title tools true twitter twittervia ui_508_compliant ui_language unit unittest url urlshortening urlshorteningservice user var variable your_profile_id_here"
+    },
+    {
+      "section": "api",
+      "id": "addthis.addthisTool",
+      "shortName": "addthis.addthisTool",
+      "type": "directive",
+      "moduleName": "addthis",
+      "shortDescription": "Use this directive to add an inline AddThis tool onto your page",
+      "keywords": "add addthis addthis-tool addthis_sharing_toolbox addthistool adipiscing aecm allow amet api button check class clicks consectetur crawls debugger define description directive dolor elit example facebook finds graph href html http https image inline ipsum lorem note open png services share share-description share-media share-title share-url shared sharedescription sharemedia sharetitle shareurl sharing sit string tags test text title tool tool-class toolclass url visitor"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api/addthis.addthisTool",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-57c460aaf72cda39"
+    ],
+    "styles": [
+      "css/doc.css"
+    ],
+    "title": "documentation",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "image": "img/logo.png",
+    "imageLink": "https://www.addthis.com",
+    "titleLink": "/docs/",
+    "analytics": {
+      "account": "UA-1170033-28"
+    },
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api/addthis.addthisTool",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js",
+    "https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-57c460aaf72cda39"
+  ]
+};
