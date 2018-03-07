2a4e6ff21c8645f6abf8c483c8a8d0bed6a8b5f0
Kirk Elliott <kirk.elliott@oracle.com>
2018-03-07 10:49:46 -0500

    using actual sample response adding a test removing unused var

--- test/example3/AddAnotherImageCtrlTest.js
+++ test/example3/AddAnotherImageCtrlTest.js
@@ -4,7 +4,113 @@ describe('appExample3 AddAnotherImageCtrl', function() {
     var $httpBackend;
     var $scope;
     var theCatApiUrl = 'https://thecatapi.com/api/images/get?format=xml&results_per_page=20&size=med&type=gif';
-    var theCatApiResponse = '<response>\n<data>\n<images>\n<image>\n<url>\nhttps://thecatapi.com/api/images/get.php?id=1aq&size=med\n</url>\n<id>1aq</id>\n<source_url>http://thecatapi.com/?id=1aq</source_url>\n</image>\n<image>\n<url>\nhttps://thecatapi.com/api/images/get.php?id=MTUwNTk4NQ&size=med\n</url>\n<id>MTUwNTk4NQ</id>\n<source_url>http://thecatapi.com/?id=MTUwNTk4NQ</source_url>\n</image>\n<image>\n<url>\nhttps://thecatapi.com/api/images/get.php?id=7v&size=med\n</url>\n<id>7v</id>\n<source_url>http://thecatapi.com/?id=7v</source_url>\n</image>\n</data>\n</response>';
+    var theCatApiResponse = '<?xml version="1.0"?>\n' +
+        '<response>\n' +
+        '  <data>\n' +
+        '    <images>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=3hq&amp;size=med</url>\n' +
+        '        <id>3hq</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=3hq</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=81p&amp;size=med</url>\n' +
+        '        <id>81p</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=81p</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=4rt&amp;size=med</url>\n' +
+        '        <id>4rt</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=4rt</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=b7r&amp;size=med</url>\n' +
+        '        <id>b7r</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=b7r</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=83k&amp;size=med</url>\n' +
+        '        <id>83k</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=83k</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=4il&amp;size=med</url>\n' +
+        '        <id>4il</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=4il</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=41f&amp;size=med</url>\n' +
+        '        <id>41f</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=41f</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=48k&amp;size=med</url>\n' +
+        '        <id>48k</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=48k</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=4h2&amp;size=med</url>\n' +
+        '        <id>4h2</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=4h2</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=33i&amp;size=med</url>\n' +
+        '        <id>33i</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=33i</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=8vj&amp;size=med</url>\n' +
+        '        <id>8vj</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=8vj</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=897&amp;size=med</url>\n' +
+        '        <id>897</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=897</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=4lf&amp;size=med</url>\n' +
+        '        <id>4lf</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=4lf</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=cme&amp;size=med</url>\n' +
+        '        <id>cme</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=cme</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=584&amp;size=med</url>\n' +
+        '        <id>584</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=584</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=3p1&amp;size=med</url>\n' +
+        '        <id>3p1</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=3p1</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=4ec&amp;size=med</url>\n' +
+        '        <id>4ec</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=4ec</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=agc&amp;size=med</url>\n' +
+        '        <id>agc</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=agc</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=4h4&amp;size=med</url>\n' +
+        '        <id>4h4</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=4h4</source_url>\n' +
+        '      </image>\n' +
+        '      <image>\n' +
+        '        <url>https://thecatapi.com/api/images/get.php?id=4ir&amp;size=med</url>\n' +
+        '        <id>4ir</id>\n' +
+        '        <source_url>http://thecatapi.com/?id=4ir</source_url>\n' +
+        '      </image>\n' +
+        '    </images>\n' +
+        '  </data>\n' +
+        '</response>\n';
     var theCatApiRequestHandler;
 
     beforeEach(function() {
@@ -21,7 +127,7 @@ describe('appExample3 AddAnotherImageCtrl', function() {
 
         $httpBackend.when('GET', 'ExampleList.html').respond({});
 
-        var controller = $controller('AddAnotherImageCtrl', { $scope: $scope });
+        $controller('AddAnotherImageCtrl', { $scope: $scope });
 
         $httpBackend.expectGET(theCatApiUrl);
         $httpBackend.expectGET('ExampleList.html').respond(200);
@@ -33,13 +139,16 @@ describe('appExample3 AddAnotherImageCtrl', function() {
         $httpBackend.verifyNoOutstandingRequest();
     });
 
-    it('should have an image queue', function() {
-        expect(Array.isArray($scope.shownImages)).toBe(true);
+  it('should initialize with arrays for imageQueue and shownImages', function() {
+    expect(Array.isArray($scope.imageQueue)).toBe(true);
+    expect(Array.isArray($scope.shownImages)).toBe(true);
+  });
+
+    it('should have an image queue with more than zero items', function() {
         expect($scope.imageQueue.length).toBeGreaterThan(0);
     });
 
     it('should initialize with one visible image', function() {
-        expect(Array.isArray($scope.shownImages)).toBe(true);
         expect($scope.shownImages.length).toBe(1);
     });
 
